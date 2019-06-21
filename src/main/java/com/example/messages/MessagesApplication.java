b513b90e70685529f286008bfe27eb81cff79d8b
pzombade <prashant_zombade@bmc.com>
2019-06-21 16:55:18 +0530

    Added a log while application start-up

--- src/main/java/com/example/messages/MessagesApplication.java
+++ src/main/java/com/example/messages/MessagesApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessagesApplication {
 
 	public static void main(String[] args) {
-		System.out.println("In Main...");
+		System.out.println("Starting the MessagesApplication");
 		SpringApplication.run(MessagesApplication.class, args);
 	}
-
 }
