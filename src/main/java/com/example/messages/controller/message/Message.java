dc15c1eee86cfa0ac1753b4d0f67d629d6ae79b6
pzombade <prashant_zombade@bmc.com>
2019-06-27 11:51:24 +0530

    Introduced hashCode and equals method to compare objects.

--- src/main/java/com/example/messages/controller/message/Message.java
+++ src/main/java/com/example/messages/controller/message/Message.java
@@ -1,5 +1,7 @@
 package com.example.messages.controller.message;
 
+import java.util.Objects;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,5 +16,24 @@ public class Message {
 	public Message(int id, String message) {
 		this.id = id;
 		this.message = message;
+	}
+	
+	@Override
+	public int hashCode() {
+		return message.hashCode();
+	}
+	
+	
+	@Override
+	public boolean equals(Object m2) {
+		Message m = (Message) m2;
+		
+		if(this == m) {
+			return true;
+		}else if(!Objects.isNull(m) && this.id == m.id && this.message == m.message) {
+			return true;
+		}
+		
+		return false;
 	}
 }
